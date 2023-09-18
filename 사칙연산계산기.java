14e7584d56d519410988c4a0b06d2d5c5d96a2b0
ingeon2 <63644383+ingeon2@users.noreply.github.com>
2023-09-18 17:38:28 +0900

    Update and rename 사칙연산계산기 to 사칙연산계산기.java

---
+++ 사칙연산계산기.java
@@ -0,0 +1,93 @@
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = "4+2-4/2*3";
+        StringTokenizer st = new StringTokenizer(s, "+-/*", true);
+        ArrayList<String> arr = new ArrayList<>();
+
+        while(st.hasMoreTokens()) {
+            arr.add(st.nextToken());
+        }
+        calc1(arr);
+        System.out.println(arr.toString());
+        System.out.println(calc2(arr));
+
+
+
+    }
+    static ArrayList<String> calc1 (ArrayList<String> arr) {
+
+        for(int i = 0 ; i < arr.size() ; i++) {
+            String op = arr.get(i);
+
+            if(op.equals("*")) {
+                int num1 = Integer.parseInt(arr.get(i-1));
+                int num2 = Integer.parseInt(arr.get(i+1));
+                int num3 = num1*num2;
+
+                arr.remove(i-1);
+                arr.remove(i-1);
+                arr.remove(i-1);
+                arr.add(i-1, String.valueOf(num3));
+                i--;
+            }
+            else if(op.equals("/")) {
+                int num1 = Integer.parseInt(arr.get(i-1));
+                int num2 = Integer.parseInt(arr.get(i+1));
+                int num3 = num1/num2;
+
+                arr.remove(i-1);
+                arr.remove(i-1);
+                arr.remove(i-1);
+                arr.add(i-1, String.valueOf(num3));
+                i--;
+            }
+        }
+
+        return arr;
+    }
+
+
+
+
+    static int calc2 (ArrayList<String> arr) {
+
+        for(int i = 0 ; i < arr.size() ; i++) {
+            String op = arr.get(i);
+
+            if(op.equals("+")) {
+                int num1 = Integer.parseInt(arr.get(i-1));
+                int num2 = Integer.parseInt(arr.get(i+1));
+                int num3 = num1+num2;
+
+                arr.remove(i-1);
+                arr.remove(i-1);
+                arr.remove(i-1);
+                arr.add(i-1, String.valueOf(num3));
+                i--;
+            }
+            else if(op.equals("-")) {
+                int num1 = Integer.parseInt(arr.get(i-1));
+                int num2 = Integer.parseInt(arr.get(i+1));
+                int num3 = num1-num2;
+
+                arr.remove(i-1);
+                arr.remove(i-1);
+                arr.remove(i-1);
+                arr.add(i-1, String.valueOf(num3));
+                i--;
+            }
+        }
+
+        return Integer.parseInt(arr.get(0));
+    }
+
+
+}
